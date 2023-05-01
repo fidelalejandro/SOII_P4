d258734f151349b15e076b4b54969772dfa35d70
fidelalejandro <92455037+fidelalejandro@users.noreply.github.com>
2023-05-01 10:39:58 +0200

    Update consumidor_LIFO.c
    funciona

--- consumidor_LIFO.c
+++ consumidor_LIFO.c
@@ -1,3 +1,79 @@
-//
-// Created by alejandro on 25/04/23.
-//
+#include <time.h> //Para la semilla que nos permitirá a partir del tiempo obtener valores aleatorios
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <mqueue.h> //Para el correcto envío de mensajes con las distintas funciones POSIX
+#include <unistd.h> //Para las llamadas a la función sleep
+
+//Constantes: Tamaño del array, número de mensajes a enviar, primer parámetros de los mq_open(const char*) y colores
+#define MAX_BUFFER 5
+#define DATOS_A_CONSUMIR 100
+#define ALMACEN1 "/ALMACEN1"
+#define ALMACEN2 "/ALMACEN2"
+
+/* cola de entrada de mensajes para el productor */
+mqd_t almacen1;
+/* cola de entrada de mensajes para el consumidor */
+mqd_t almacen2;
+
+//Función del consumidor
+void consumidor(void){
+    char mensaje;
+    int i;
+    //Mensajes vaciós
+    for(int i=0;i<MAX_BUFFER;i++) {
+        mensaje='w';
+        mq_send(almacen1,&mensaje,sizeof(char),1);
+        printf("se han enviado\n");
+    }
+
+    sleep(1); //Introducimos un sleep para esperar a que el productor llene la cola luego de que el consumidor haya hecho lo mismo
+
+    unsigned int prioridad;
+    for(i=0; i < DATOS_A_CONSUMIR; i++){
+        mq_receive(almacen2,&mensaje,sizeof(char),&prioridad);
+        printf("\n(C): Se ha recibido --%c--\n", mensaje);
+        mq_send(almacen1,&mensaje,sizeof(char),0);
+        printf("(C): Se ha consumido --%c--\n", mensaje);
+    }
+
+    printf("Se han consumido todos los datos\n");
+    for(i=0;i<MAX_BUFFER;i++){
+        mq_receive(almacen1,&mensaje,sizeof(char),&prioridad);
+    }
+
+}
+
+void main(void) {
+    struct mq_attr attr; /* Atributos de la cola */
+    attr.mq_maxmsg = MAX_BUFFER;
+    attr.mq_msgsize = sizeof (char);
+
+    /* Apertura de los buffers */
+    almacen1 = mq_open("/ALMACEN1", O_RDWR, 0777, &attr);
+    almacen2 = mq_open("/ALMACEN2", O_RDONLY, 0777, &attr);
+
+    if ((almacen1 == -1) || (almacen2 == -1)) {
+        perror ("mq_open");
+        exit(EXIT_FAILURE);
+    }
+
+    srand(time(NULL));
+
+    consumidor();
+
+    mq_getattr(almacen2, &attr);
+    printf("A la cola le quedan C:%ld\n",attr.mq_curmsgs);
+    mq_getattr(almacen1, &attr);
+    printf("A la cola le quedan P:%ld\n",attr.mq_curmsgs);
+
+    mq_close(almacen1);
+    mq_close(almacen2);
+
+    //Se destruyen las colas
+    mq_unlink("/ALMACEN1");
+    mq_unlink("/ALMACEN2");
+
+    exit(EXIT_SUCCESS);
+}
